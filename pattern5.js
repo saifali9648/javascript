0f21d496e59912e8293d888e814da19d14aad12d
saifali9648 <sali964876@gmail.com>
2023-09-16 23:39:53 +0530

    pattern

---
+++ pattern5.js
@@ -0,0 +1,21 @@
+var p="";
+for(var i=1;i<=5;i++){
+    for(var j=1;j<i;j++){
+        p+=" ";
+    }
+    for(var k=i;k<=5;k++){
+        p+=k;
+    }
+    p+="\n";
+}
+console.log(p);
+for(let a=5;a<=1;a--){
+    for(let b=1;b<a;b++){
+        p+=" ";
+    }
+    for(let c=a;c<=5;c++){
+        p+=c;
+    }
+    p+="\n";
+}
+console.log(p);
---
+++ pattern6.js
@@ -0,0 +1,11 @@
+let p="";
+for(let a=5;a<=1;a--){
+    for(let b=1;b<a;b++){
+        p+=" ";
+    }
+    for(let c=a;c<=5;c--){
+        p+=c;
+    }
+    p+="\n";
+}
+console.log(p);
