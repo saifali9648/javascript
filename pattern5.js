5bf5da7d07d7f263b817225b3686ddde58167495
saifali9648 <sali964876@gmail.com>
2023-09-16 23:55:26 +0530

    pattern

--- pattern5.js
+++ pattern5.js
@@ -9,13 +9,3 @@ for(var i=1;i<=5;i++){
     p+="\n";
 }
 console.log(p);
-for(let a=5;a<=1;a--){
-    for(let b=1;b<a;b++){
-        p+=" ";
-    }
-    for(let c=a;c<=5;c++){
-        p+=c;
-    }
-    p+="\n";
-}
-console.log(p);
